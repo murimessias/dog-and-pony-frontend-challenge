64a13c4a720e99a7fd3fff97fd3b2f2b5e2e38ac
Murilo Messias de Camargo <murilo.messias@gmail.com>
2023-05-09 10:01:22 -0300

    feat: add theme shadow token

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,17 @@ module.exports = {
       fontFamily: {
         sans: ['var(--font-roboto)', ...fontFamily.sans],
       },
+      boxShadow: {
+        'extra-large': '0px 25px 50px -10px rgba(0, 0, 0, 0.25)',
+        base: '0px 10px 15px -5px rgba(0, 0, 0, 0.1), 0px 4px 6px rgba(0, 0, 0, 0.05)',
+        inner: 'inset 0px 2px 4px rgba(0, 0, 0, 0.06)',
+        large:
+          '0px 20px 25px -10px rgba(0, 0, 0, 0.1), 0px 10px 10px rgba(0, 0, 0, 0.04)',
+        micro:
+          '0px 1px 2px rgba(0, 0, 0, 0.06), 0px 1px 3px rgba(0, 0, 0, 0.1)',
+        small:
+          '0px 2px 4px -5px rgba(0, 0, 0, 0.06), 0px 4px 6px rgba(0, 0, 0, 0.1)',
+      },
     },
   },
   plugins: [],
