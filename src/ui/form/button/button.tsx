6f8b7504aee133cbfa342971cd83d100634e5ca4
Murilo Messias de Camargo <murilo.messias@gmail.com>
2023-05-10 08:44:26 -0300

    style: update buttom component styles

--- src/ui/form/button/button.tsx
+++ src/ui/form/button/button.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { ComponentProps } from 'react'
 import clsx from 'clsx'
 
@@ -20,14 +21,14 @@ export const Button = ({
   return (
     <button
       className={clsx(
-        'inline-flex min-h-[40px] w-fit min-w-[7ch] items-center justify-center gap-2 rounded px-4 py-2 text-center font-normal outline-none transition-colors  disabled:cursor-not-allowed disabled:bg-primary-grey disabled:text-white',
+        'inline-flex w-fit min-w-[7ch] items-center justify-center gap-2 rounded px-4 py-2 text-center font-normal outline-none transition-colors  disabled:cursor-not-allowed disabled:bg-primary-grey disabled:text-white',
         fullWidth && 'w-full',
-        size === 'base' && 'text-base',
-        size === 'sm' && 'text-sm',
+        size === 'base' && 'min-h-[40px] text-base',
+        size === 'sm' && 'px-2 py-1 text-sm leading-none tracking-[0.02em]',
         variant === 'attention' &&
-          'uppercase tracking-[0.02em] text-accent-red hover:ring-2 hover:ring-accent-red hover:ring-offset-1 focus:ring-2 focus:ring-accent-red focus:ring-offset-2 disabled:uppercase',
+          'uppercase  text-accent-red hover:ring-2 hover:ring-accent-red hover:ring-offset-1 focus:ring-2 focus:ring-accent-red focus:ring-offset-2 disabled:uppercase',
         variant === 'neutral' &&
-          'uppercase tracking-[0.02em] text-primary-grey hover:ring-2 hover:ring-primary-grey hover:ring-offset-1 focus:ring-2 focus:ring-primary-grey focus:ring-offset-2 disabled:uppercase',
+          'uppercase text-primary-grey hover:ring-2 hover:ring-primary-grey hover:ring-offset-1 focus:ring-2 focus:ring-primary-grey focus:ring-offset-2 disabled:uppercase',
         variant === 'primary' &&
           'bg-accent-blue text-white hover:bg-primary-dark-blue focus:ring-2 focus:ring-accent-blue focus:ring-offset-2',
       )}
@@ -36,7 +37,7 @@ export const Button = ({
       {!!leftAddon && (
         <div className='flex items-center justify-center'>{leftAddon}</div>
       )}
-      {children}
+      <div>{children}</div>
     </button>
   )
 }
